b8572237f31a64e6a75830e1360845023ea20114
chydbd <azaz_chy@163.com>
2024-04-28 09:09:35 +0800

    栈语句实现完成

--- Stack_homework/Stack_homework/Stack_homework.cpp
+++ Stack_homework/Stack_homework/Stack_homework.cpp
@@ -12,7 +12,34 @@ typedef struct {
 
 int InitStack(SqStack& S) {
     S.base = (int*)malloc(STACK_INIT_SIZE * sizeof(int));
+    if (!S.base)exit(-2);
+    S.top = S.base;
+    S.stacksize = STACK_INIT_SIZE;
+    return 1;
+}
 
+int GetTop(SqStack S, int& e) {
+    if (S.top == S.base)return 0;
+    e = *(S.top - 1);
+    return 1;
+}
+
+int Push(SqStack& S, int& e) {
+    if (S.top - S.base >= S.stacksize) {
+        S.base = (int*)realloc(S.base,
+            (S.stacksize + STACKINCREMENT) * sizeof(int));
+        if (!S.base)exit(-2);
+        S.top = S.base + S.stacksize;
+        S.stacksize += STACKINCREMENT;
+    }
+    *S.top++ = e;
+    return 1;
+}
+
+int Pop(SqStack& S, int e) {
+    if (S.top == S.base)return 0;
+    e = *--S.top;
+    return 1;
 }
 
 int main()
