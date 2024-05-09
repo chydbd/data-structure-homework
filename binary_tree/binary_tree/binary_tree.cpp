7d15a7547f88efb65aa8ee335e7a2beeafc90b55
chydbd <azaz_chy@163.com>
2024-05-09 09:23:27 +0800

    biTree ongoing

--- binary_tree/binary_tree/binary_tree.cpp
+++ binary_tree/binary_tree/binary_tree.cpp
@@ -4,32 +4,32 @@
 #define STACK_INIT_SIZE 100
 #define STACKINCREMENT 10
 
-typedef int TElemtype;
+typedef char TElemtype;
 
 typedef struct {
-    int* base;
-    int* top;
+    BiTree* base;
+    BiTree* top;
     int stacksize;
 }SqStack;
 
 int InitStack(SqStack& S) {
-    S.base = (int*)malloc(STACK_INIT_SIZE * sizeof(int));
+    S.base = (BiTree*)malloc(STACK_INIT_SIZE * sizeof(BiTree));
     if (!S.base)exit(-2);
     S.top = S.base;
     S.stacksize = STACK_INIT_SIZE;
     return 1;
 }
 
-int GetTop(SqStack S, int& e) {
+int GetTop(SqStack S, BiTree& e) {
     if (S.top == S.base)return 0;
     e = *(S.top - 1);
     return 1;
 }
 
-int Push(SqStack& S, int& e) {
+int Push(SqStack& S, BiTree& e) {
     if (S.top - S.base >= S.stacksize) {
-        S.base = (int*)realloc(S.base,
-            (S.stacksize + STACKINCREMENT) * sizeof(int));
+        S.base = (BiTree*)realloc(S.base,
+            (S.stacksize + STACKINCREMENT) * sizeof(BiTree));
         if (!S.base)exit(-2);
         S.top = S.base + S.stacksize;
         S.stacksize += STACKINCREMENT;
@@ -38,14 +38,19 @@ int Push(SqStack& S, int& e) {
     return 1;
 }
 
-int Pop(SqStack& S, int e) {
+int Pop(SqStack& S, BiTree e) {
     if (S.top == S.base)return 0;
     e = *--S.top;
     return 1;
 }
 
+int StackEmpty(SqStack& S) {
+    if (S.base = S.top)return 1;
+    else return 0;
+}
+
 typedef struct BiTNode {
-	int data;
+	char data;
 	struct BiTNode* lchild, * rchild;
 }BiTNode, *BiTree;
 
@@ -61,6 +66,30 @@ int PreOrderTraverse(BiTree T, int (*Visit)(int e)) {
 
 int InOrderTraverse(BiTree T, int (*Visit)(int e)) {
     SqStack S;
-    InitStack(S); Push(S, T);
+    InitStack(S); BiTree p = T;
+    while (p || !StackEmpty(S)) {
+        if (p) {
+            Push(S, p);
+            p = p->lchild;
+        }
+        else {
+            Pop(S, p);
+            if (!Visit(p->data))return 0;
+            p = p->rchild;
+        }
+    }
+    return 1;
+}
 
+int CreateBiTree(BiTree& T) {
+    char ch;
+    scanf(&ch);
+    if (ch == ' ')T = NULL;
+    else {
+        if (!(T = (BiTNode*)malloc(sizeof(BiTNode))))exit(-2);
+        T->data = ch;
+        CreateBiTree(T->lchild);
+        CreateBiTree(T->rchild);
+    }
+    return 1;
 }
